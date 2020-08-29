9fd84abca943863ed90fe97efd768c1374c393d2
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-29 09:15:22 -0400

    Updates

---
+++ precache-manifest.e179f859f4d1d0c9ed47010b35094123.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a3d4f3e4e9353cb57c729b117907536",
+    "url": "/laptop_customizer/index.html"
+  },
+  {
+    "revision": "2ad72763cee9fb6879e2",
+    "url": "/laptop_customizer/static/css/main.72647eb3.chunk.css"
+  },
+  {
+    "revision": "eea433dbb22b7457235d",
+    "url": "/laptop_customizer/static/js/2.54426a57.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/laptop_customizer/static/js/2.54426a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ad72763cee9fb6879e2",
+    "url": "/laptop_customizer/static/js/main.c8802af5.chunk.js"
+  },
+  {
+    "revision": "cea92e930ef750dc4fc0",
+    "url": "/laptop_customizer/static/js/runtime-main.cff6aa8b.js"
+  }
+]);
